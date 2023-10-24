62b07f5ec98a22f0a81eef48ab68b056de047b60
Sebastian Näs <sena0220@storsjogymnasiet.se>
2023-10-24 11:08:38 +0200

    glosor

---
+++ glosor/glosor.py
@@ -0,0 +1,43 @@
+def main():
+    glosLista = {}
+
+    print("-:glosor:-")
+    antal_ratt = 0
+    glosor = {}
+
+    while True:
+        svglosa = input("\n\tMata in svensk glosa: ")
+        englosa = input("\n\tmata in engelsk glosa: ")
+
+        glosor[svglosa] = englosa
+
+        glosLista.update({svglosa : englosa})
+
+        stoppa = input("\n\tVill du mata in en glosa till? j/n: ")
+     
+        if (stoppa == "n"):
+            break
+
+    while True:
+        print("\n Nu start glosförhöret!")
+
+        antal_ratt = 0
+
+        for glosa in glosLista:
+         svar = input(f"\n {glosa} : ")
+
+         if svar == glosLista[glosa]:
+            print("Rätt svar!")
+            antal_ratt += 1
+            print(f"\n{antal_ratt}")
+         else :
+            print(f"\nFel svar, det är {glosLista[glosa]}")
+            print(f"\n{antal_ratt}")
+
+        fortsatt2 = input("Vill du köra om? j/n: ")
+        if fortsatt2 == "n":
+            break
+            
+
+
+main()
