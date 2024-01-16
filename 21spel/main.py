52e7a05d5e9fd718c246c11629e1cd4a0c18c124
Sebastian Näs <sena0220@storsjogymnasiet.se>
2024-01-16 14:10:11 +0100

    sten

--- 21spel/main.py
+++ 21spel/main.py
@@ -1,7 +1,7 @@
 
 total_summa = 0
-summa1 =
-summa2 =
+summa1 = 0
+summa2 = 0
 IS_player1 = True
 
 spel21 = ("willkommen")
@@ -14,26 +14,26 @@ while total_summa < 21:
     total_summa += player1
     if player1 > 3 or player1 < 1:
         if player1 < 3:
-        print("Too big number")
-        total_summa = 30
-    if player1 == 1 or 2 or 3
-        add to total_summa
+            print("Too big number")
+            total_summa = 30
+    if player1 == 1 or 2 or 3:
+        player1 + total_summa
+    if total_summa == 21:
+        print("player 2 wins!")
 
     player2 = int(input("player 2 will you add 1,2 or 3 to the total? "))
     total_summa += player2
     if player2 > 3 or player2 < 1:
         if player2 < 3:
-        print("Too big number")
-        total_summa = 30
-    if player2 == 1 or 2 or 3
-        add to total_summa
+            print("Too big number")
+            total_summa = 30
+    if player2 == 1 or 2 or 3:
+        player2 + total_summa
+    if total_summa == 21:
+        print("player 2 wins!")
 
+  
+    if total_summa > 21:
+        print("you botha lose nerds!!")
 
-
-    
-
-    
-if total_summa > 21:
-    print("you botha lose nerds!!")
-if total_summa = 21
     
---
+++ stensaxpåse/main.py
@@ -0,0 +1,13 @@
+import random
+Sten = 1
+Sax = 2
+Påse = 3
+int(input("Sten(1), sax(2) eller påse(3)" )) == (player)
+
+
+
+#computer code
+random(int(1,2,3))
+    if 1:
+        
+
