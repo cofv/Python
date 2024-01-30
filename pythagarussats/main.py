768bc4b8b6f4659f1739075a79335dfe84610502
Sebastian Näs <sena0220@storsjogymnasiet.se>
2024-01-30 11:25:30 +0100

    sats

---
+++ pythagarussats/main.py
@@ -0,0 +1,13 @@
+print("Ta in 3 nummer för triangel a,b,c")
+
+a = int(input())
+b = int(input())
+c = int(input())
+a2 = (a * a)
+b2 = (b * b)
+c2 = (c * c)
+
+if a2 + b2 == c2:
+    print("Fungerar")
+else:
+    print(":(")
