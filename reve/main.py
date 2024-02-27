a76c3ca487e0f825c37870dd9cfc7f683ea5018c
Sebastian Näs <sena0220@storsjogymnasiet.se>
2024-02-27 11:31:30 +0100

    deezlove

---
+++ reve/main.py
@@ -0,0 +1,14 @@
+word = input("Input word! ")
+
+word_reversed = ""
+
+
+for x in reversed(word):
+    
+
+
+
+if word == word_reversed:
+    print("Word is same backwords as it is normally")
+else:
+    print("word no same backwords")
---
+++ älskarinte/main.py
@@ -0,0 +1,9 @@
+blad = int(input("Hur många blad ska blomman din ha? "))
+
+
+
+for i in range(blad):
+    if (i % 2 == 0 ):
+        print("Älskar")
+    else:
+        print("Älskar inte")
